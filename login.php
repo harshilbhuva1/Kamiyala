8feb37ee13e9e26cafbe4d55e116657a92aec3f8
harshilbhuva1 <135381111+harshilbhuva1@users.noreply.github.com>
2023-06-03 23:47:57 +0530

    ass

--- login.php
+++ login.php
@@ -34,7 +34,7 @@
         }
     } else {
 ?>
-    <form class="form" method="post" name="login">
+    <form class="form" method="post" name="login.php">
         <h1 class="login-title">Login</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
         <input type="password" class="login-input" name="password" placeholder="Password"/>
